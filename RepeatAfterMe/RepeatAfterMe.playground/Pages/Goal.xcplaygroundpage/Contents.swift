bca6025fbbc90510fa5a73d4631a21287d446d92
Student <student@macbook-pro-0435-luke-tao.local>
2017-02-21 10:50:24 -0500

    80%done with all the circles and rectangle

--- RepeatAfterMe/RepeatAfterMe.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ RepeatAfterMe/RepeatAfterMe.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -20,19 +20,42 @@ import PlaygroundSupport
 //: ## Your code starts here
 // Create a new canvas
 let canvas = Canvas(width: 360, height: 360)
-
-// Loop three times
-for i in 1...3 {
-    
-    // Generate random number between -75 and 75
-    let offset = Int(arc4random_uniform(150)) - 75
-    
-    // Draw circles in random horizontal positions in the middle of the canvas
-    canvas.drawEllipse(centreX: canvas.width / 2 + offset, centreY: 100 * i , width: 50, height: 50)
-
+canvas.drawShapesWithBorders = true
+//middle rainbow circle.
+canvas.drawShapesWithFill = false
+for x in stride(from: 1, through: 360, by: 1){
+    canvas.borderColor = Color(hue: Float(x), saturation: 100, brightness: 90, alpha: 50)
+    canvas.drawEllipse(centreX: 180, centreY: 180, width: x, height: x)
 }
 
-// Modify code above as needed and continue writing code below as needed... 
+//cicles on top left corner
+canvas.drawShapesWithFill = false
+canvas.drawShapesWithBorders = true
+for j in stride(from: 0, through: 110, by: 10){
+canvas.borderColor = Color.black
+canvas.drawEllipse(centreX: 55, centreY: 305, width: j, height: j)
+}
+
+//right bottom rectangle
+let q = 10
+for w in 1...10{
+canvas.drawShapesWithFill = false
+canvas.drawShapesWithBorders = true
+canvas.drawRectangle(bottomLeftX: 360 - w * q, bottomLeftY: 0 + w * q, width: w * q, height: -(w * q))
+}
+//tkae out the little circles.
+//// Loop three timesyy
+//for i in 1...3 {
+//
+//    // Generate random number between -75 and 75
+//    let offset = Int(arc4random_uniform(150)) - 75
+//
+//    // Draw circles in random horizontal positions in the middle of the canvas
+//    canvas.drawEllipse(centreX: canvas.width / 2 + offset, centreY: 100 * i , width: 50, height: 50)
+//
+//}
+
+// Modify code above as needed and continue writing code below as needed...
 
 
 
